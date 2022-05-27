7faf897dfb9d7f4c03fa2648cc372d883b01860d
William Zhu <Liam.Zhu@protonmail.com>
2022-05-26 23:52:02 -0400

    In process of upgrading to state monad, created scratch code (incredibly ugly version) that needs to be refactored

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,9 @@
 module Main where
 
-import EdMonad
+import EdMonad ( ed, Mode(CommandMode) )
+import System.Console.Haskeline ( defaultSettings, runInputT )
 
 -- As described in the assignment,
 -- ed starts in command mode
 main :: IO ()
-main = ed ([], 0, CommandMode)
+main = runInputT defaultSettings $ ed (CommandMode, ([],0))
